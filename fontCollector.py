d8c8544f81cdc71a671263ad35fb2f27a26b4bd0
moi15moi <moi15moismokerlolilol@gmail.com>
2022-04-21 14:24:41 -0400

    Bump version

--- fontCollector.py
+++ fontCollector.py
@@ -15,7 +15,7 @@ from typing import List, NamedTuple, Set
 from colorama import Fore, init
 init(convert=True)
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 
 # GLOBAL VARIABLES
 LINE_PATTERN = re.compile(r"(?:\{(?P<tags>[^}]*)\}?)?(?P<text>[^{]*)")
