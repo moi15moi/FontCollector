ee626b43447b4a467bcb2a163b93897ce27eb6b2
moi15moi <moi15moismokerlolilol@gmail.com>
2023-01-03 20:56:43 -0500

    [setup.py] Remove python 3.7

--- setup.py
+++ setup.py
@@ -76,7 +76,6 @@ setuptools.setup(
         "Intended Audience :: Other Audience",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
