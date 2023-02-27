8c21b55bc8adf969afde58c5327e13a34414f54b
moi15moi <moi15moismokerlolilol@gmail.com>
2023-02-26 21:09:21 -0500

    [tests\test_font] Add test for get_missing_glyphs

--- tests/test_font.py
+++ tests/test_font.py
@@ -1,10 +1,12 @@
 import os
+import string
 from font_collector import Font
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-font_without_os2_table = os.path.join(dir_path, "fonts", "BRUSHSTP.TTF")
+font_without_os2_table = os.path.join(dir_path, "fonts", "font_mac.TTF")
 font_without_stat_table = os.path.join(dir_path, "fonts", "Cabin VF Beta Regular.ttf")
-font_without_axis_value = os.path.join(dir_path, "fonts", "font without axis_value.ttf")
+font_without_axis_value = os.path.join(dir_path, "fonts", "font_without axis_value.ttf")
+font_cmap_encoding_2 = os.path.join(dir_path, "fonts", "font_cmap_encoding_2.TTF")
 
 
 def test_font_without_os2_table():
@@ -12,8 +14,6 @@ def test_font_without_os2_table():
     font = Font.from_font_path(font_without_os2_table)
     assert len(font) == 1
     font = font[0]
-
-    print(font)
 
     assert font.family_names == set(["brushstroke plain"])
     assert font.weight == 400
@@ -47,3 +47,21 @@ def test_font_without_axis_value():
     assert font.italic == False
     assert font.exact_names == set([])
     assert font.is_var == True
+
+def test_font_get_missing_glyphs_cmap_encoding_2():
+
+    font = Font.from_font_path(font_cmap_encoding_2)
+    assert len(font) == 1
+    font = font[0]
+
+    missing_glyphs = font.get_missing_glyphs(string.ascii_letters + string.digits + "éｦ&*")
+    assert missing_glyphs == set(["é"])
+
+def test_font_get_missing_glyphs_cmap_encoding_mac_platform():
+
+    font = Font.from_font_path(font_without_os2_table)
+    assert len(font) == 1
+    font = font[0]
+
+    missing_glyphs = font.get_missing_glyphs(string.ascii_letters + string.digits + "@é¸")
+    assert missing_glyphs == set(["@", "¸"])
