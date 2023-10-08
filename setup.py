97e85af52c73aa4da24a40f646950657980d7ec1
moi15moi <moi15moismokerlolilol@gmail.com>
2023-10-08 17:40:34 -0400

    [setup] Bump FindSystemFontsFilename version to 0.1.2

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setuptools.setup(
         "ass-tag-analyzer",
         "fontTools>=4.38.0",
         "freetype-py",
-        "FindSystemFontsFilename>=0.1.1",
+        "FindSystemFontsFilename>=0.1.2",
     ],
     entry_points={"console_scripts": ["fontcollector=font_collector.__main__:main"]},
     classifiers=[
