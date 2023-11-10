a1cb3bef2395b87065cad20609e482bcd9815eab
moi15moi <moi15moismokerlolilol@gmail.com>
2023-11-10 17:01:47 -0500

    [test_ass_document] Add test to cover duplicate style name

--- tests/test_ass_document.py
+++ tests/test_ass_document.py
@@ -24,6 +24,7 @@ def test_get_style_used():
 
     assert styles == expected_results
 
+
 def test_get_style_used_with_and_without_collect_draw_fonts():
     path_ass = os.path.join(dir_path, "ass", "Collect Draw Style test.ass")
     subtitle = AssDocument.from_file(path_ass)
@@ -38,5 +39,49 @@ def test_get_style_used_with_and_without_collect_draw_fonts():
     expected_results = {
         AssStyle("Jester", 700, False): UsageData(set("Test"), {1}),
         AssStyle("testgmail", 700, False): UsageData(set(), {2}),
+    }
+    assert styles == expected_results
+
+
+def test_get_style_used_empty_line_style_name():
+    path_ass = os.path.join(dir_path, "ass", "Empty line style name.ass")
+    subtitle = AssDocument.from_file(path_ass)
+
+    styles = subtitle.get_used_style()
+    expected_results = {
+        AssStyle("Verdana", 700, False): UsageData(set("Test"), {1}),
+    }
+    assert styles == expected_results
+
+
+def test_get_style_used_2_duplicate_style_name():
+    path_ass = os.path.join(dir_path, "ass", "2 duplicate style name.ass")
+    subtitle = AssDocument.from_file(path_ass)
+
+    styles = subtitle.get_used_style()
+    expected_results = {
+        AssStyle("Jester", 700, False): UsageData(set("Jester"), {1}),
+    }
+    assert styles == expected_results
+
+
+def test_get_style_used_3_duplicate_style_name():
+    path_ass = os.path.join(dir_path, "ass", "3 duplicate style name.ass")
+    subtitle = AssDocument.from_file(path_ass)
+
+    styles = subtitle.get_used_style()
+    expected_results = {
+        AssStyle("Jester", 700, False): UsageData(set("Jester"), {1}),
+    }
+    assert styles == expected_results
+
+
+def test_get_style_used_2_duplicate_style_name_with_number():
+    path_ass = os.path.join(dir_path, "ass", "2 duplicate style name with number.ass")
+    subtitle = AssDocument.from_file(path_ass)
+
+    styles = subtitle.get_used_style()
+    expected_results = {
+        AssStyle("Raleway", 700, False): UsageData(set("Raleway"), {1}),
     }
     assert styles == expected_results
