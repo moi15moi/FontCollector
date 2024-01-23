fc0079369fd2cdbd4e940cf1e78c34d0949f5010
moi15moi <80980684+moi15moi@users.noreply.github.com>
2024-01-23 18:37:01 -0500

    [system_lang\linux_lang] Add a exception when getlocale is equal to "C"

--- font_collector/system_lang/linux_lang.py
+++ font_collector/system_lang/linux_lang.py
@@ -6,6 +6,7 @@ class LinuxLang(ABCSystemLang):
     @staticmethod
     def get_lang() -> str:
         lang, _ = getlocale()
-        if lang is None:
+        # C means that the locale isn't set. See: https://docs.python.org/3/library/locale.html#locale.getlocale
+        if lang is None or lang == "C":
             lang = ABCSystemLang.default_lang
         return lang
