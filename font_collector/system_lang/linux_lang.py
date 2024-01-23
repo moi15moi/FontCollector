9b6d68f50e2d1a7d274dfaea23ea9f2ae66bafc6
moi15moi <80980684+moi15moi@users.noreply.github.com>
2024-01-22 21:04:44 -0500

    [system_lang\linux_lang] Replace getdefaultlocale by getlocale
    getdefaultlocale is deprecated since 3.11

--- font_collector/system_lang/linux_lang.py
+++ font_collector/system_lang/linux_lang.py
@@ -1,11 +1,11 @@
 from .abc_system_lang import ABCSystemLang
-from locale import getdefaultlocale
+from locale import getlocale
 
 class LinuxLang(ABCSystemLang):
 
     @staticmethod
     def get_lang() -> str:
-        lang, _ = getdefaultlocale()
+        lang, _ = getlocale()
         if lang is None:
             lang = ABCSystemLang.default_lang
         return lang
