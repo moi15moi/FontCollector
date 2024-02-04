9731ed790cd7a9d3021dcf676fa0659d6122e63a
moi15moi <80980684+moi15moi@users.noreply.github.com>
2024-02-04 11:19:07 -0500

    [font\font_selection_strategy_libass] Add is_font_name_match

--- font_collector/font/font_selection_strategy_libass.py
+++ font_collector/font/font_selection_strategy_libass.py
@@ -1,7 +1,7 @@
 from ..ass.ass_style import AssStyle
 from .abc_font_face import ABCFontFace
 from .font_selection_strategy import FontSelectionStrategy
-from .font_type import FontType
+
 
 __all__ = ["FontSelectionStrategyLibass"]
 
@@ -19,6 +19,21 @@ class FontSelectionStrategyLibass(FontSelectionStrategy):
     Note: The Windows shared algorithm from 1992 may have been modified since then.
     """
 
+    def is_font_name_match(self, font_face: ABCFontFace, style_font_name: str) -> bool:
+        """
+        Args:
+            font_face: A FontFace
+            style_font_name: A font name. It can come from a AssStyle
+        Returns:
+            True if the style_font_name match with the font_face. Otherwise, false.
+        """
+        family_name_match = any(style_font_name.lower() == family_name.value.lower() for family_name in font_face.family_names)
+
+        if not family_name_match:
+            exact_name_match = any(style_font_name.lower() == exact_name.value.lower() for exact_name in font_face.exact_names)
+        
+        return family_name_match or exact_name_match
+
 
     def need_faux_bold(self, font_face: ABCFontFace, style: AssStyle) -> bool:
         return style.weight > font_face.weight + 150 and not font_face.is_glyph_emboldened
@@ -27,12 +42,8 @@ class FontSelectionStrategyLibass(FontSelectionStrategy):
     def get_similarity_score(self, font_face: ABCFontFace, style: AssStyle) -> float:
         score = 0.0
 
-        family_name_match = any(style.fontname.lower() == family_name.value.lower() for family_name in font_face.family_names)
-
-        if not family_name_match:
-            exact_name_match = any(style.fontname.lower() == exact_name.value.lower() for exact_name in font_face.exact_names)
-            if not exact_name_match:
-                return float("inf")
+        if not self.is_font_name_match(font_face, style.fontname):
+            return float("inf")
 
         if style.italic and not font_face.is_italic:
             score += 1
