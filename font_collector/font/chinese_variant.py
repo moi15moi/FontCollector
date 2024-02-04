d0cfbe9f4789210551fb3e6df09188104f275835
moi15moi <80980684+moi15moi@users.noreply.github.com>
2024-02-04 13:07:42 -0500

    [font\chinese_variant] Add comment to describe where is the logic is taken from

--- font_collector/font/chinese_variant.py
+++ font_collector/font/chinese_variant.py
@@ -13,6 +13,8 @@ class ChineseVariant(Enum):
         if lang_code.language != "zh":
             raise ValueError(f"The language {lang_code} isn't chinese.")
 
+        # This logic is taken from the LCID v15.
+        # See: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/70feba9f-294e-491e-b6eb-56532684c37f
         if lang_code.territory in ("TW", "HK", "MO") or lang_code.script == "Hant":
             return ChineseVariant.TRADITIONAL
         return ChineseVariant.SIMPLIFIED
