moi15moi/FontCollector

commit 661f009fb60217c8975e65a8898e2393f9cb306d

Add mkvfontvalidator
Close https://github.com/moi15moi/FontCollector/issues/49

Breaking changes:
- Mkvpropedit.delete_fonts_of_mkv have been renamed Mkvpropedit.delete_all_fonts_of_mkv
- Mkvpropedit.path doesn't exist anymore. Now, use MKVUtils.MKVTOOLNIX_FOLDER to set the folder of mkvtoolnix.
- Mkvpropedit class have been renamed MKVPropedit.

font_collector/__main__.py
@@ -5,15 +5,14 @@ from sys import argv
 
 from . import _handler
 from .ass.ass_document import AssDocument
+from .collect_fonts import collect_subtitle_fonts
 from .font import (
     FontCollection,
     FontFile,
     FontLoader,
-    FontSelectionStrategyLibass,
-    VariableFontFace,
-    font_weight_to_name
+    FontSelectionStrategyLibass
 )
-from .mkvpropedit import Mkvpropedit
+from .mkvtoolnix.mkvpropedit import MKVPropedit
 from .parse_arguments import parse_arguments
 
 _logger = logging.getLogger(__name__)
@@ -50,62 +49,14 @@ def main() -> None:
         for ass_path in ass_files_path:
             subtitle = AssDocument.from_file(ass_path)
             _logger.info(f"Loaded successfully {ass_path}")
-            used_styles = subtitle.get_used_style(collect_draw_fonts)
 
-            nbr_font_not_found = 0
-
-            for style, usage_data in used_styles.items():
-
-                font_result = font_collection.get_used_font_by_style(style, font_strategy)
-
-                # Did not found the font
-                if font_result is None:
-                    nbr_font_not_found += 1
-                    _logger.error(
-                        f"Could not find font '{style.fontname}'\n"
-                        f"Used on lines: {' '.join(str(line) for line in usage_data.ordered_lines)}"
-                    )
-                else:
-                    log_msg = ""
-                    if font_result.need_faux_bold:
-                        log_msg = f"Faux bold used for '{style.fontname}' (requested weight {style.weight}-{(font_weight_to_name(style.weight))}, got {font_result.font_face.weight}-{(font_weight_to_name(font_result.font_face.weight))})."
-                    elif font_result.mismatch_bold:
-                        log_msg = f"Mismatched weight for '{style.fontname}' (requested weight {style.weight}-{(font_weight_to_name(style.weight))}, got {font_result.font_face.weight}-{(font_weight_to_name(font_result.font_face.weight))})."
-                    if font_result.mismatch_italic:
-                        log_msg = f"Mismatched italic for '{style.fontname}' (requested {'' if style.italic else 'non-'}italic, got {'' if font_result.font_face.is_italic else 'non-'}italic)."
-
-                    if log_msg:
-                        _logger.warning(
-                            f"{log_msg}\n"
-                            f"Used on lines: {' '.join(str(line) for line in usage_data.ordered_lines)}"
-                        )
-
-
-                    missing_glyphs = font_result.font_face.get_missing_glyphs(usage_data.characters_used)
-                    if len(missing_glyphs) > 0:
-                        _logger.warning(f"'{style.fontname}' is missing the following glyphs used: {missing_glyphs}")
-
-
-                    if font_result.font_face.font_file is None:
-                        raise ValueError(f"This font_face \"{font_result.font_face}\" isn't linked to any FontFile.")
-
-                    if convert_variable_to_collection and isinstance(font_result.font_face, VariableFontFace):
-                        font_name = font_result.font_face.get_best_family_prefix_from_lang().value
-                        font_filename = output_directory.joinpath(f"{font_name}.ttc")
-                        generated_font_file = font_result.font_face.variable_font_to_collection(font_filename)
-                        fonts_file_found.add(generated_font_file)
-                    else:
-                        fonts_file_found.add(font_result.font_face.font_file)
-
-            if nbr_font_not_found == 0:
-                _logger.info(f"All font(s) found")
-            else:
-                _logger.error(f"{nbr_font_not_found} font(s) could not be found.")
+            fonts_file_found.update(collect_subtitle_fonts(subtitle, font_collection, font_strategy, collect_draw_fonts, convert_variable_to_collection, output_directory))
+            _logger.info("")
 
         if mkv_path is not None:
             if delete_fonts:
-                Mkvpropedit.delete_fonts_of_mkv(mkv_path)
-            Mkvpropedit.merge_fonts_into_mkv(fonts_file_found, mkv_path)
+                MKVPropedit.delete_all_fonts_of_mkv(mkv_path)
+            MKVPropedit.merge_fonts_into_mkv(fonts_file_found, mkv_path)
         else:
             if not output_directory.is_dir():
                 output_directory.mkdir()

examples/collect_font_and_mux_them.py
@@ -8,7 +8,7 @@ from font_collector import (
     FontFile,
     FontLoader,
     FontSelectionStrategyLibass,
-    Mkvpropedit,
+    MKVPropedit,
     set_loglevel
 )
 
@@ -47,9 +47,9 @@ def main():
 
 
     # If the mkv already contains font, you can remove them
-    Mkvpropedit.delete_fonts_of_mkv(mkv_path)
+    MKVPropedit.delete_all_fonts_of_mkv(mkv_path)
 
-    Mkvpropedit.merge_fonts_into_mkv(fonts_file_found, mkv_path)
+    MKVPropedit.merge_fonts_into_mkv(fonts_file_found, mkv_path)
 
 
 if __name__ == "__main__":

font_collector/__init__.py
@@ -3,10 +3,10 @@ import logging
 # Packages
 from .ass import *
 from .font import *
+from .mkvtoolnix import *
 from .system_lang import *
 # Files
 from .exceptions import *
-from .mkvpropedit import *
 from ._version import __version__
 from fontTools.misc.loggingTools import configLogger
 
@@ -16,6 +16,9 @@ class IndentMultilineFormatter(logging.Formatter):
         super().__init__("%(levelname)s - %(message)s")
 
     def format(self, record: logging.LogRecord) -> str:
+        if not record.msg:
+            return ""
+
         s = super().format(record)
         head, *tail = s.splitlines()
         indent = " " * (len(record.levelname) + 3)  # "LEVEL - " length

font_collector/collect_fonts.py
@@ -1,0 +1,98 @@
+import logging
+from pathlib import Path
+from typing import Optional
+
+from .ass.ass_document import AssDocument
+from .font import (
+    FontCollection,
+    FontFile,
+    FontSelectionStrategy,
+    VariableFontFace,
+    font_weight_to_name
+)
+
+_logger = logging.getLogger(__name__)
+
+
+def collect_subtitle_fonts(
+        subtitle: AssDocument,
+        font_collection: FontCollection,
+        font_strategy: FontSelectionStrategy,
+        collect_draw_fonts: bool,
+        convert_variable_to_collection: bool = False,
+        output_variable_font_directory: Optional[Path] = None
+    ) -> set[FontFile]:
+    """
+    Collect the fonts used in a given subtitle (ASS) document.
+
+    Args:
+        subtitle (AssDocument): The ASS subtitle document to be analyzed.
+        font_collection (FontCollection): The collection of available fonts that will be used to match against the subtitle's styles.
+        font_strategy (FontSelectionStrategy): The strategy used to select the best matching font for each style.
+        collect_draw_fonts (bool): Whether to include fonts used in ASS drawing commands (`\\pN` commands).
+        convert_variable_to_collection (bool): If True, variable fonts found in the subtitle will be converted into
+            a TrueType Collection (TTC) file and written to the specified output directory.
+        output_variable_font_directory (Optional[Path]): The directory where converted variable fonts will be saved, if applicable.
+            Required when `convert_variable_to_collection` is True.
+
+    Returns:
+        A set of `FontFile` objects representing all fonts used by the ASS document.
+    """
+    fonts_file_found: set[FontFile] = set()
+
+    used_styles = subtitle.get_used_style(collect_draw_fonts)
+
+    nbr_font_not_found = 0
+
+    for style, usage_data in used_styles.items():
+
+        font_result = font_collection.get_used_font_by_style(style, font_strategy)
+
+        # Did not found the font
+        if font_result is None:
+            nbr_font_not_found += 1
+            _logger.error(
+                f"Could not find font '{style.fontname}'\n"
+                f"Used on lines: {' '.join(str(line) for line in usage_data.ordered_lines)}"
+            )
+        else:
+            log_msg = ""
+            if font_result.need_faux_bold:
+                log_msg = f"Faux bold used for '{style.fontname}' (requested weight {style.weight}-{(font_weight_to_name(style.weight))}, got {font_result.font_face.weight}-{(font_weight_to_name(font_result.font_face.weight))})."
+            elif font_result.mismatch_bold:
+                log_msg = f"Mismatched weight for '{style.fontname}' (requested weight {style.weight}-{(font_weight_to_name(style.weight))}, got {font_result.font_face.weight}-{(font_weight_to_name(font_result.font_face.weight))})."
+            if font_result.mismatch_italic:
+                log_msg = f"Mismatched italic for '{style.fontname}' (requested {'' if style.italic else 'non-'}italic, got {'' if font_result.font_face.is_italic else 'non-'}italic)."
+
+            if log_msg:
+                _logger.warning(
+                    f"{log_msg}\n"
+                    f"Used on lines: {' '.join(str(line) for line in usage_data.ordered_lines)}"
+                )
+
+
+            missing_glyphs = font_result.font_face.get_missing_glyphs(usage_data.characters_used)
+            if len(missing_glyphs) > 0:
+                _logger.warning(f"'{style.fontname}' is missing the following glyphs used: {missing_glyphs}")
+
+
+            if font_result.font_face.font_file is None:
+                raise ValueError(f"This font_face \"{font_result.font_face}\" isn't linked to any FontFile.")
+
+            if convert_variable_to_collection and isinstance(font_result.font_face, VariableFontFace):
+                if output_variable_font_directory is None:
+                    raise ValueError("When ``convert_variable_to_collection`` is True, you must provide a value for ``output_variable_font_directory``.")
+                font_name = font_result.font_face.get_best_family_prefix_from_lang().value
+                font_filename = output_variable_font_directory.joinpath(f"{font_name}.ttc")
+                generated_font_file = font_result.font_face.variable_font_to_collection(font_filename)
+                fonts_file_found.add(generated_font_file)
+            else:
+                fonts_file_found.add(font_result.font_face.font_file)
+
+    if nbr_font_not_found == 0:
+        _logger.info(f"All font(s) found")
+    else:
+        _logger.error(f"{nbr_font_not_found} font(s) could not be found.")
+
+    return fonts_file_found
+

font_collector/mkvfontvalidator.py
@@ -1,0 +1,127 @@
+import logging
+from argparse import ArgumentParser
+from datetime import datetime
+from pathlib import Path
+from sys import argv
+from tempfile import TemporaryDirectory
+from typing import Optional
+
+from . import _handler
+from .ass.ass_document import AssDocument
+from .collect_fonts import collect_subtitle_fonts
+from .font import (
+    FontCollection,
+    FontFile,
+    FontLoader,
+    FontSelectionStrategyLibass
+)
+from .mkvtoolnix import MKVExtract, MKVFontFile, MKVPropedit, MKVUtils
+
+_logger = logging.getLogger(__name__)
+
+
+def main() -> None:
+    start_time = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+    default_log_path = Path.cwd().joinpath(f"{start_time}_mkvfontvalidator.log")
+
+    parser = ArgumentParser(
+        description="MKV font validator for Advanced SubStation Alpha file."
+    )
+    parser.add_argument(
+        "-mkv",
+        type=Path,
+        required=True,
+        help="""
+    The video file to be verified. Must be a Matroska file.
+    """,
+    )
+    parser.add_argument(
+        "-mkvtoolnix",
+        type=Path,
+        help="""
+    Path to the MKVToolNix folder if not in variable environments.
+    """,
+    )
+    parser.add_argument(
+        "--need-draw-fonts",
+        action="store_true",
+        help="""
+        If specified, MKVFontValidator will report a error if a font used in a draw isn't muxed to the mkv. For more detail when this is usefull, see: https://github.com/libass/libass/issues/617
+    """,
+    )
+    parser.add_argument(
+        "--delete-fonts-not-used",
+        "-d",
+        action="store_true",
+        help="""
+        If specified, MKVFontValidator will remove the fonts that aren't used by the subtitle(s) of the mkv file.
+    """,
+    )
+    parser.add_argument(
+        "--logging",
+        "-log",
+        type=Path,
+        nargs="?",
+        const=default_log_path,
+        default=None,
+        help="""
+    Destination path of log. If it isn't specified, it will be YYYY-MM-DD--HH-MM-SS_mkvfontvalidator.log.
+    """,
+    )
+
+    args = parser.parse_args()
+
+    mkv_path: Path = args.mkv
+    need_draw_fonts: bool = args.need_draw_fonts
+    delete_fonts_not_used: bool = args.delete_fonts_not_used
+    logging_file_path: Optional[Path] = args.logging
+
+    if args.mkvtoolnix:
+        MKVUtils.MKVTOOLNIX_FOLDER = args.mkvtoolnix
+
+    if logging_file_path:
+        file_handler = logging.FileHandler(logging_file_path, mode="a", encoding="utf-8")
+        file_handler.setLevel(_handler.level)
+        file_handler.setFormatter(_handler.formatter)
+        root_logger = logging.getLogger()
+        root_logger.addHandler(file_handler)
+        _logger.info(f"{Path.cwd()}>{' '.join(argv)}")
+
+    try:
+        with TemporaryDirectory() as tmp_dir:
+
+            mkv_ass_files = MKVExtract.get_mkv_ass_files(mkv_path, Path(tmp_dir))
+            mkv_font_files = MKVExtract.get_mkv_font_files(mkv_path, Path(tmp_dir))
+
+            fonts_file_found: set[FontFile] = set()
+            additional_fonts = FontLoader.load_additional_fonts([f.filename for f in mkv_font_files])
+            font_collection = FontCollection(use_system_font=False, use_generated_fonts=False, additional_fonts=additional_fonts)
+            font_strategy = FontSelectionStrategyLibass()
+
+            for mkv_ass_file in mkv_ass_files:
+                subtitle = AssDocument.from_file(mkv_ass_file.filename)
+                log_msg = f"Loaded successfully the .ass stream at index {mkv_ass_file.mkv_id}"
+                if mkv_ass_file.track_name:
+                    log_msg += f" - \"{mkv_ass_file.track_name}\""
+                _logger.info(log_msg)
+
+                fonts_file_found.update(collect_subtitle_fonts(subtitle, font_collection, font_strategy, need_draw_fonts, False, None))
+                _logger.info("")
+
+            font_not_used: list[MKVFontFile] = []
+            for mkv_font_file in mkv_font_files:
+                if not any(mkv_font_file.filename.samefile(font_file.filename) for font_file in fonts_file_found):
+                    font_not_used.append(mkv_font_file)
+
+                    if not delete_fonts_not_used:
+                        _logger.warning(f"You can remove the font at the id {mkv_font_file.mkv_id} named \"{mkv_font_file.mkv_font_filename}\". It is not used by any .ass file.")
+
+            if delete_fonts_not_used and font_not_used:
+                MKVPropedit.delete_fonts_of_mkv(mkv_path, [mkv_font.mkv_id for mkv_font in font_not_used])
+                _logger.info(f'Successfully deleted {", ".join([f"{mkv_font.mkv_id}-{mkv_font.mkv_font_filename}" for mkv_font in font_not_used])} of the mkv "{mkv_path}"')
+    except Exception as e:
+        _logger.error("An unexpected error occured", exc_info=True)
+
+
+if __name__ == "__main__":
+    main()

font_collector/mkvpropedit.py
@@ -1,135 +1,0 @@
-import logging
-import subprocess
-from collections.abc import Iterable
-from enum import IntEnum
-from pathlib import Path
-from shutil import which
-from typing import Optional
-
-from .font.font_file import FontFile
-
-__all__ = ["Mkvpropedit"]
-_logger = logging.getLogger(__name__)
-
-
-class MkvpropeditExitCode(IntEnum):
-    # From https://mkvtoolnix.download/doc/mkvpropedit.html#d4e1266
-    SUCCESS = 0
-    WARNING = 1
-    ERROR = 2
-
-
-class Mkvpropedit:
-    """
-    This class is a collection of static methods that will help
-    the user to interact with mkvpropedit.
-    """
-
-    path: Optional[str] = which("mkvpropedit")
-
-    @staticmethod
-    def is_mkv(filename: Path) -> bool:
-        """
-        Args:
-            filename: The file name.
-        Returns:
-            True if the mkv is an mkv file, false in any others cases
-        """
-
-        if not filename.is_file():
-            raise FileNotFoundError(f'The file "{filename.resolve()}" does not exist.')
-
-        with open(filename, "rb") as f:
-            # From https://en.wikipedia.org/wiki/List_of_file_signatures
-            return f.read(4) == b"\x1a\x45\xdf\xa3"
-
-
-    @staticmethod
-    def delete_fonts_of_mkv(mkv_filename: Path) -> None:
-        """Delete all mkv attached font
-
-        Args:
-            mkv_filename: Path to mkv file.
-        """
-
-        if Mkvpropedit.path is None:
-            raise FileNotFoundError(
-                f'"{Mkvpropedit.path}" is not an valid path for Mkvpropedit. You need to correct your environnements variable or change the value of Mkvpropedit.path'
-            )
-
-        if not Mkvpropedit.is_mkv(mkv_filename):
-            raise FileExistsError(f'The file "{mkv_filename}" is not an mkv file.')
-
-        # We only want to remove ttf, ttc or otf file
-        # This is from mpv: https://github.com/mpv-player/mpv/blob/c438732b239bf4e7f3d574f8fcc141f92366018a/sub/sd_ass.c#L133-L145
-
-        mkvpropedit_args = [
-            Mkvpropedit.path,
-            str(mkv_filename.resolve()),
-            "--delete-attachment", "mime-type:application/x-truetype-font",
-            "--delete-attachment", "mime-type:application/vnd.ms-opentype",
-            "--delete-attachment", "mime-type:application/x-font-otf",
-            "--delete-attachment", "mime-type:application/x-font-ttf",
-            "--delete-attachment", "mime-type:application/x-font",
-            "--delete-attachment", "mime-type:application/font-sfnt",
-            "--delete-attachment", "mime-type:font/collection",
-            "--delete-attachment", "mime-type:font/otf",
-            "--delete-attachment", "mime-type:font/sfnt",
-            "--delete-attachment", "mime-type:font/ttf",
-            "--command-line-charset", "UTF-8",
-            "--output-charset", "UTF-8",
-        ]
-
-        output = subprocess.run(mkvpropedit_args, capture_output=True, text=True, encoding="utf-8")
-        exit_code = MkvpropeditExitCode(output.returncode)
-
-        if exit_code == MkvpropeditExitCode.ERROR:
-            raise OSError(
-                f"mkvpropedit reported an error when deleting the font in the mkv: {output.stdout}."
-            )
-        elif exit_code == MkvpropeditExitCode.WARNING:
-            _logger.warning(f"mkvpropedit reported an warning when deleting the font in the mkv '{output.stdout}'.")
-        elif exit_code == MkvpropeditExitCode.SUCCESS:
-            _logger.info(f'Successfully deleted fonts in mkv "{mkv_filename}.')
-
-
-    @staticmethod
-    def merge_fonts_into_mkv(
-        fonts_file: Iterable[FontFile],
-        mkv_filename: Path,
-    ) -> None:
-        """Merge font file into the mkv.
-
-        Args:
-            fonts_file: All font file that will be merge to the mkv
-            mkv_filename: Path to mkv file.
-        """
-        if Mkvpropedit.path is None:
-            raise FileNotFoundError(
-                f'"{Mkvpropedit.path}" is not an valid path for Mkvpropedit. You need to correct your environnements variable or change the value of Mkvpropedit.path'
-            )
-
-        if not Mkvpropedit.is_mkv(mkv_filename):
-            raise FileExistsError(f'The file "{mkv_filename}" is not an mkv file.')
-
-        mkvpropedit_args = [
-            Mkvpropedit.path,
-            str(mkv_filename.resolve()),
-            "--command-line-charset", "UTF-8",
-            "--output-charset", "UTF-8",
-        ]
-
-        for font_file in fonts_file:
-            mkvpropedit_args.extend(['--add-attachment', str(font_file.filename.resolve())])
-
-        output = subprocess.run(mkvpropedit_args, capture_output=True, text=True, encoding="utf-8")
-        exit_code = MkvpropeditExitCode(output.returncode)
-
-        if exit_code == MkvpropeditExitCode.ERROR:
-            raise OSError(
-                f"mkvpropedit reported an error when merging font into an mkv: {output.stdout}"
-            )
-        elif exit_code == MkvpropeditExitCode.WARNING:
-            _logger.warning(f'mkvpropedit reported an warning when merging font into an mkv "{mkv_filename}"')
-        elif exit_code == MkvpropeditExitCode.SUCCESS:
-            _logger.info(f'Successfully merged fonts into mkv "{mkv_filename}"')

font_collector/mkvtoolnix/__init__.py
@@ -1,0 +1,7 @@
+from .exit_code import *
+from .mkv_ass_file import *
+from .mkv_font_file import *
+from .mkv_utils import *
+from .mkvextract import *
+from .mkvmerge import *
+from .mkvpropedit import *

font_collector/mkvtoolnix/exit_code.py
@@ -1,0 +1,11 @@
+from enum import IntEnum
+
+__all__ = ["ExitCode"]
+
+
+class ExitCode(IntEnum):
+    # From https://mkvtoolnix.download/doc/mkvpropedit.html#d4e1266
+    # All the MKVToolNix programs use this enumeration
+    SUCCESS = 0
+    WARNING = 1
+    ERROR = 2

commit 6c2778291280f99e865662f550a8b9c8eaea7a6d

[__main__] Add the file_handler to the root logger
Without this, the file logger doesn't register all the log.
Fix https://github.com/moi15moi/FontCollector/issues/88
Introduced in https://github.com/moi15moi/FontCollector/pull/82

font_collector/__main__.py
@@ -38,7 +38,8 @@ def main() -> None:
         file_handler = logging.FileHandler(logging_file_path, mode="a", encoding="utf-8")
         file_handler.setLevel(_handler.level)
         file_handler.setFormatter(_handler.formatter)
-        _logger.addHandler(file_handler)
+        root_logger = logging.getLogger()
+        root_logger.addHandler(file_handler)
         _logger.info(f"{Path.cwd()}>{' '.join(argv)}")
 
     try: