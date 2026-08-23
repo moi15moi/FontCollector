8a5aefd15b747a27e6d2c67ea7aacc9cb54d0d8a
moi15moi <moi15moismokerlolilol@gmail.com>
2026-08-23 15:26:02 -0400

    Add test_font_get_missing_glyphs_cmap_legacy_arabic_simplified

--- tests/font/test_normal_font_face.py
+++ tests/font/test_normal_font_face.py
@@ -172,6 +172,18 @@ def test_font_get_missing_glyphs_cmap_encoding_2():
     assert missing_glyphs == {"é"}
 
 
+def test_font_get_missing_glyphs_cmap_legacy_arabic_simplified():
+    font_path = Path(os.path.join(os.path.dirname(dir_path), "file", "fonts", "legacy-arabic-simplified-SimplifiedArabic.ttf"))
+    font_file = FontFile.from_font_path(font_path)
+
+    assert len(font_file.font_faces) == 1
+    font_face = font_file.font_faces[0]
+    assert isinstance(font_face, NormalFontFace)
+
+    missing_glyphs = font_face.get_missing_glyphs("جa")
+    assert missing_glyphs == set("a")
+
+
 def test_font_get_missing_glyphs_cmap_encoding_mac_platform():
     font_mac_platform = Path(os.path.join(os.path.dirname(dir_path), "file", "fonts", "font_mac.TTF"))
     font_file = FontFile.from_font_path(font_mac_platform)
